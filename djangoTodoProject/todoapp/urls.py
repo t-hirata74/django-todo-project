29e043b030cd55d654437d8c59a386960619cc09
t-hirata74 <t.hirata.0463@gmail.com>
2023-06-28 23:52:30 +0900

    TaskDetailの作成

--- djangoTodoProject/todoapp/urls.py
+++ djangoTodoProject/todoapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
-    path("", TaskList.as_view())
+    path("", TaskList.as_view()),
+    path("task/<int:pk>/", TaskDetail.as_view())  # <int:pk>は動的パス, pkはプライマリーキー
 ]
--- djangoTodoProject/todoapp/views.py
+++ djangoTodoProject/todoapp/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from todoapp.models import Task
 
@@ -11,3 +12,8 @@ from todoapp.models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = "tasks"
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = "task"
