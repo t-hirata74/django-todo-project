b0e70fc0f84be94dfd6bafa23b5563dfe526a36a
t-hirata74 <t.hirata.0463@gmail.com>
2023-06-28 23:11:22 +0900

    クラスベースviewの書き方にする

--- djangoTodoProject/todoapp/views.py
+++ djangoTodoProject/todoapp/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
+from todoapp.models import Task
+
+# def taskList(request):
+#     return HttpResponse("<h1>Hello Django</h1>")
 
 
-def taskList(request):
-    return HttpResponse("<h1>Hello Django</h1>")
+class TaskList(ListView):
+    model = Task
