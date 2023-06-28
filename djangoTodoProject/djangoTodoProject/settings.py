95515578c4ad3ae523028d44a5f178cf31674869
t-hirata74 <t.hirata.0463@gmail.com>
2023-06-28 23:16:40 +0900

    templatesを表示できるように設定

--- djangoTodoProject/djangoTodoProject/settings.py
+++ djangoTodoProject/djangoTodoProject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoTodoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
