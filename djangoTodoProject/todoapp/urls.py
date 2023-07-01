6ee77b879f7deac2bfec9a77acf32118a7c3d6a8
t-hirata74 <t.hirata.0463@gmail.com>
2023-07-01 18:33:16 +0900

    CreateViewでタスクを作成するフォームを設定

--- djangoTodoProject/todoapp/urls.py
+++ djangoTodoProject/todoapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import TaskList, TaskDetail
+from .views import TaskCreate, TaskList, TaskDetail
 
 urlpatterns = [
-    path("", TaskList.as_view()),
+    path("", TaskList.as_view(), name="tasks"),
     path("task/<int:pk>/", TaskDetail.as_view(),
-         name="task")  # <int:pk>は動的パス, pkはプライマリーキー
+         name="task"),  # <int:pk>は動的パス, pkはプライマリーキー
+    path("create-task/", TaskCreate.as_view(), name="create-task")
 ]
--- djangoTodoProject/todoapp/views.py
+++ djangoTodoProject/todoapp/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 
 from todoapp.models import Task
 
@@ -17,3 +19,9 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = "task"
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = "__all"  # modelsのtaskのfieldを一括で宣言できる
+    success_url = reverse_lazy("tasks")  # urlsのtasksにルートされる
